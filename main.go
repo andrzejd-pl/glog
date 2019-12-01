4c5a5940a13ec3b5720b39dbc3484afe8a1d337b
andrzej <dybowski@andrzejd.pl>
2019-12-01 18:59:43 +0100

    write simple http server

--- main.go
+++ main.go
@@ -2,48 +2,17 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 const storagePath string = "./storage/"
 
-type Page struct {
-	Title string
-	Body  []byte
-}
-
-func (p *Page) save() error {
-	filename := storagePath + p.Title + ".txt"
-	return ioutil.WriteFile(filename, p.Body, 0600)
-}
-
-func loadPage(title string) (*Page, error) {
-	filename := storagePath + title + ".txt"
-	body, err := ioutil.ReadFile(filename)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return &Page{
-		Title: title,
-		Body:  body,
-	}, nil
-}
-
-func CheckIfError(err error) {
-	if err != nil {
-		_ = fmt.Errorf("error: %s", err.Error())
-	}
+func handler(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 func main() {
-	p1 := Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-	err := p1.save()
-	CheckIfError(err)
-
-	p2, err := loadPage("TestPage")
-	CheckIfError(err)
-
-	fmt.Println(string(p2.Body))
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
