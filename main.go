c519264e0aee6eef0aa54168e1c348a4f42075b1
andrzej <dybowski@andrzejd.pl>
2019-12-01 19:41:21 +0100

    extend http server

--- main.go
+++ main.go
@@ -2,17 +2,51 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 const storagePath string = "./storage/"
+const viewUrlPath string = "/view/"
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	_, _ = fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := storagePath + p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := storagePath + title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Page{
+		Title: title,
+		Body:  body,
+	}, nil
+}
+
+func CheckIfError(err error) {
+	if err != nil {
+		_ = fmt.Errorf("error: %s", err.Error())
+	}
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len(viewUrlPath):]
+	p, _ := loadPage(title)
+	_, _ = fmt.Fprintf(w, "<h1>%s</h1><div>%s</dir>", p.Title, p.Body)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc(viewUrlPath, viewHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
