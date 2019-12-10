3b7fe5052ac9c2b497ea9d4582615497440d8ffb
Andrzej Dybowski <dybowski@andrzejd.pl>
2019-12-10 20:43:32 +0100

    add another test set with empty data and example data

--- api/posts_test.go
+++ api/posts_test.go
@@ -5,9 +5,11 @@ import (
 	"github.com/andrzejd-pl/glog/repository"
 	"io/ioutil"
 	"log"
+	"math/big"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 func TestGetAllPosts(t *testing.T) {
@@ -20,10 +22,28 @@ func TestGetAllPosts(t *testing.T) {
 		want http.HandlerFunc
 	}{
 		{
-			name: "Empty test",
+			name: "Nil repository test",
 			args: struct{ repository repository.PostRepository }{repository: nil},
 			want: func(writer http.ResponseWriter, request *http.Request) {
 				http.Error(writer, ResourceNotFound, http.StatusInternalServerError)
+			},
+		},
+		{
+			name: "Empty resource test",
+			args: struct{ repository repository.PostRepository }{repository: mockPostRepository(repository.Posts{}, nil)},
+			want: func(writer http.ResponseWriter, request *http.Request) {
+				writer.WriteHeader(http.StatusOK)
+				_, _ = writer.Write([]byte("[]"))
+			},
+		},
+		{
+			name: "Example data test",
+			args: struct{ repository repository.PostRepository }{repository: mockPostRepository(repository.Posts{
+				stubPost("123456", 10, "Test", "Test Content", "2019-12-31 00:00:00", "2006-01-02 15:04:05"),
+			}, nil)},
+			want: func(writer http.ResponseWriter, request *http.Request) {
+				writer.WriteHeader(http.StatusOK)
+				_, _ = writer.Write([]byte("[{\"Uuid\":123456,\"Title\":\"Test\",\"Content\":\"Test Content\",\"Timestamp\":\"2019-12-31T00:00:00Z\"}]"))
 			},
 		},
 	}
@@ -54,5 +74,33 @@ func TestGetAllPosts(t *testing.T) {
 				t.Errorf("got body %s want %s", gotBody, wantBody)
 			}
 		})
+	}
+}
+
+type PostRepositoryMock struct {
+	posts repository.Posts
+	err   error
+}
+
+func (repo PostRepositoryMock) GetAllPosts() (repository.Posts, error) {
+	return repo.posts, repo.err
+}
+
+func mockPostRepository(posts repository.Posts, err error) repository.PostRepository {
+	return PostRepositoryMock{
+		posts: posts,
+		err:   err,
+	}
+}
+
+func stubPost(uuidValue string, uuidBase int, title, content, timestampValue, timestampFormat string) repository.Post {
+	uuid := big.Int{}
+	uuid.SetString(uuidValue, uuidBase)
+	timestamp, _ := time.Parse(timestampFormat, timestampValue)
+	return repository.Post{
+		Uuid:      uuid,
+		Title:     title,
+		Content:   content,
+		Timestamp: timestamp,
 	}
 }
